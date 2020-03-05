ac8d25972e0968bf4444962c36bb7047b7ca7dc4
ZakCrissinger <zcris001@plattsburgh.edu>
2020-03-05 10:41:46 -0500

    added maps button signs out and goes to map

--- app/src/main/java/com/example/plattsmap/MapsActivity.java
+++ app/src/main/java/com/example/plattsmap/MapsActivity.java
@@ -40,7 +40,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
+        LatLng sydney = new LatLng(44.6930116, -73.4653251);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
